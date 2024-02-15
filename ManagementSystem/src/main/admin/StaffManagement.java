12763458954cd4941959880727e90a51f4bd9640
Rahim-Rahmatzada <abdul.rahmatzada@city.ac.uk>
2024-02-15 18:06:44 +0000

    Completed first initial interfaces

--- ManagementSystem/src/main/admin/StaffManagement.java
+++ ManagementSystem/src/main/admin/StaffManagement.java
@@ -1,4 +1,16 @@
 package main.admin;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public interface StaffManagement {
+    void scheduleStaff(String staffName, Date scheduleDate);
+    Map<Date, List<String>> getStaffSchedules();
+    void recordHoliday(String staffName, Date startDate, Date endDate);
+    void updateStaffRole(String staffName, String newRole);
+    void recordAbsences(List<String> staffNames, List<Date> startDates, List<Date> endDates);
+
+
 }
+
--- ManagementSystem/src/main/admin/WineManagement.java
+++ ManagementSystem/src/main/admin/WineManagement.java
@@ -1,4 +1,12 @@
 package main.admin;
 
+import java.util.Map;
+
 public interface WineManagement {
+    void updateWineStock(String wineName, String wineType , int quantity);
+    Map<String, Integer> getWineStockLevels();
+    void suggestWinePairing(String dishName, String wineName, String wineType);
+    Map<String, Integer> reportWineUsage(String wineType);
+
 }
+
