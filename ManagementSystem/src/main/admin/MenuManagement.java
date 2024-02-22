bdaa729009a1338a5fe8b7e178e793c104b58236
love-eze <love.eze@city.ac.uk>
2024-02-22 18:52:05 +0000

    Implementation

--- ManagementSystem/src/main/admin/MenuManagement.java
+++ ManagementSystem/src/main/admin/MenuManagement.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public interface MenuManagement {
     void createOrUpdateMenu(String dishName, List<String> ingredients, double price);
     Map<String, Double> getMenuPricing();
+    void setDishPrice (String dishName, List<String> ingredients);
     void updateIngredientCost(String ingredient, double cost);
     void setAllergenInfo(String dishName, List<String> allergens);
     void archiveMenu(String menuName);
